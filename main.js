c21d56010c64bc427900b69ad29a64d96257c8c1
juliaeduar <163419295+juliaeduar@users.noreply.github.com>
2024-03-15 10:06:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://s2-oglobo.glbimg.com/1J1dzRMeszo_8Y4_GMWoPkQJ4FI=/0x0:2000x1194/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2023/z/K/93KA9tR7GaPikXLuAw3A/arte-11-.png";
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Barbie_Logo.svg/1200px-Barbie_Logo.svg.png"
+}
